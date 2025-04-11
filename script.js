8b3b215d282fa199d48fb1f98287d050aef22b90
amiracle10 <amiracle10@ivytech.edu>
2025-04-11 16:18:49 -0500

    updat url again

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // we are going to mkae an event listener.. it will trigger with the DOM is loaded (aka upon visting webpage)
 addEventListener("DOMContentLoaded", async function(){
-    const response = await this.fetch("https://soapy-fair-snapdragon.glitch.me/")
+    const response = await this.fetch("https://quintessential-petite-ghost.glitch.me/api/songs")
     const songs = await response.json()
 
     let html = ""
