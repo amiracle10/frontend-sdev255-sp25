ef4d68aad5f7911e40ba05c1ffb9fa7ebed2ddba
amiracle10 <amiracle10@ivytech.edu>
2025-04-26 18:40:08 -0500

    front and backend

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// we are going to mkae an event listener.. it will trigger with the DOM is loaded (aka upon visting webpage)
+addEventListener("DOMContentLoaded", async function(){
+    const response = await this.fetch("http://localhost:3000/api/songs")
+    const songs = await response.json()
+
+    let html = ""
+    for (let song of songs){
+        let songID = song._id
+        html+=`<li>${song.title} - ${song.artist} - <a href= "details.html?id=${songID}">Details</a> - <a href= "edit.html?id=${songID}">Edit Song</a></li>`
+    }
+
+    document.querySelector("#list_of_songs").innerHTML = html
+})
+
+//https://quintessential-petite-ghost.glitch.me/api/songs
