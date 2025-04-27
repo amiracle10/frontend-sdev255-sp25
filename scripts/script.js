94aaeaaf33598af291d784945f6bfdaef4a37f14
amiracle10 <amiracle10@ivytech.edu>
2025-04-26 19:42:17 -0500

    nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // we are going to mkae an event listener.. it will trigger with the DOM is loaded (aka upon visting webpage)
 addEventListener("DOMContentLoaded", async function(){
-    const response = await this.fetch("https://inexpensive-classy-gallon.glitch.me/api/songs")
+    const response = await this.fetch("https://inexpensive-classy-gallon.glitch.me/api/songs/")
     const songs = await response.json()
 
     let html = ""
